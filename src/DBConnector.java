7259a5ca381c94062f0a54fc72b60c23f91478d6
yunahamazawa <yunahamazawa821@gmail.com>
2024-12-18 22:19:58 +0900

    DBConnector修正

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -2,10 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a66c388de86e02c8f305251124978fe4782a8190
+
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
 
@@ -16,7 +14,7 @@ public class DBConnector {
 	private static String user = "root";
 	private static String password = "mysql";
 
-<<<<<<< HEAD
+
 	public Connection getConnection() {
 		Connection con = null;
 
@@ -30,19 +28,4 @@ public class DBConnector {
 		}
 		return con ;
 	}
-=======
-public Connection getConnection() {
-Connection con = null;
-
-try{
-Class.forName(driverName);
-con = DriverManager.getConnection(url,user,password);
-} catch (ClassNotFoundException e) {
-e.printStackTrace() ;
-} catch (SQLException e) {
-e.printStackTrace() ;
-}
-return con ;
-}
->>>>>>> a66c388de86e02c8f305251124978fe4782a8190
 }
