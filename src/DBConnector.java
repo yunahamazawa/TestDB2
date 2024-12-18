2a48f63fa81f47a4793c2e816de593bb5a9b8175
yunahamazawa <yunahamazawa821@gmail.com>
2024-12-18 22:16:27 +0900

    DBConnector作成

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
 public class DBConnector {
 	private static String driverName = "com.mysql.jdbc.Driver";
 
