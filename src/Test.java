936567278a602f55b225b272751ae0901e4b523e
yunahamazawa <yunahamazawa821@gmail.com>
2025-01-02 23:47:43 +0900

    all表示

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[] args) {
 		TestUserDAO dao = new TestUserDAO();
 		dao.select("taro", "123");
+		dao.selectAll();
 	}
 }
--- src/TestUserDAO.java
+++ src/TestUserDAO.java
@@ -33,4 +33,29 @@ public class TestUserDAO {
 			 e.printStackTrace();
 		 }
 	}
+
+	 public void selectAll() {
+		 DBConnector db = new DBConnector();
+		 Connection con = db.getConnection();
+
+
+		 String sql ="select * from test_table";
+		 try {
+			 PreparedStatement ps = con.prepareStatement(sql);
+			 ResultSet rs=ps.executeQuery();
+			 while (rs.next()) {
+				 System.out.println(rs.getString("user_name"));
+				 System.out.println(rs.getString("password"));
+			 }
+		 	} catch (SQLException e ) {
+		 		e.printStackTrace();
+		 	}
+		 try {
+			 con.close() ;
+		 } catch (SQLException e ) {
+			 e.printStackTrace();
+		 }
+	 }
 }
+
+
